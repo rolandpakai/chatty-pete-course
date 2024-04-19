632bc7f762f76e3b953c58dfa977c1754ea9126b
roland.pakai <roland.pakai@lightware.com>
2024-04-19 17:01:55 +0200

    SF

--- components/AuthType/AvatarSelector/AvatarSelector.js
+++ components/AuthType/AvatarSelector/AvatarSelector.js
@@ -35,16 +35,16 @@ export const AvatarSelector = ({ setSelectedAvatar }) => {
         {avatarList.map((avatar, index) => (
             <div key={index} className="m-2 relative">
               <Image
-                  src={avatar.src}
-                  name={avatar.name}
-                  alt={''}
-                  width={40}
-                  height={40}
-                  objectFit="contain"
-                  className={`icon cursor-pointer hover:scale-150 transition-transform duration-200 ${
-                    selectedAvatar === avatar.name ? 'scale-150 border-2 border-custom-green' : ''
-                  }`}
-                  onClick={(e) => handleOnClick(avatar)}
+                src={avatar.src}
+                name={avatar.name}
+                alt={''}
+                width={40}
+                height={40}
+                objectFit="contain"
+                className={`icon cursor-pointer hover:scale-150 transition-transform duration-200 ${
+                  selectedAvatar === avatar.name ? 'scale-150 border-2 border-custom-green' : ''
+                }`}
+                onClick={(e) => handleOnClick(avatar)}
               />
             </div>
           ))}
