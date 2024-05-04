0b4c1b1e4a12f5d9bb4053281c73aa379be7f0fb
roland.pakai <roland.pakai@lightware.com>
2024-05-04 12:12:47 +0200

    Change items-center to items-start

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -53,7 +53,7 @@ export const Message = ({ env, role, content }) => {
 
   return (
     <div className={`grid grid-cols-[30px_1fr] gap-5 p-5 ${role === 'assistant' ? 'bg-gray-600' : role === 'notice' ? 'bg-red-600' : ''}`}>
-      <div className="flex items-center justify-center">
+      <div className="flex items-start justify-center">
         {role === 'user' && !!user && (
           <Image 
             src={user.picture} 
