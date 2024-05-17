045fb24ca4437da96f0ab06c67c8f5b6376464d5
roland.pakai <roland.pakai@lightware.com>
2024-05-17 16:25:33 +0200

    Handle file role and content

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -4,7 +4,7 @@ import { useUser } from "lib/auth/client";
 import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { faRobot, faFile } from "@fortawesome/free-solid-svg-icons";
 import Slider from "react-slick";
 import Markdown from 'react-markdown'
 import Cookies from 'universal-cookie';
@@ -98,40 +98,60 @@ export const Message = ({ env, role, content }) => {
   };
 
   useEffect(() => {
-    const { contentWithoutImages, images } = extractImagesFromMarkdown(content);
-    setImages(images);
-    setMarkdownContent(contentWithoutImages);
-  }, [content]);
+    switch (role) {
+      case 'user': {
+        setMarkdownContent(content);
+        setDisplayName(user.name);
+        break;
+      }
+      case 'assistant': {
+        const { contentWithoutImages, images } = extractImagesFromMarkdown(content);
+        setImages(images);
+        setMarkdownContent(contentWithoutImages);
+        setDisplayName(env.AI_NAME);
+        break;
+      }
+      case 'file': {
+        if (content) {
+          try {
+            const fileJson = JSON.parse(content);
+            const fileContent = `
+            Type: ${fileJson.type}
+            Size: ${fileJson.size} bytes
+            Last Modified: ${new Date(fileJson.lastModified).toLocaleString('en-UK')}`;
 
-  useEffect(() => {
-    let displayName = '?';
-
-    if (role === 'user' && !!user) {
-      displayName = user.name;
+            setMarkdownContent(fileContent);
+            setDisplayName(fileJson.name);
+          } catch (e) {
+            setMarkdownContent(`Not a valid JSON: ${content}`);
+          }
+        }
+        break;
+      }
+      default: setMarkdownContent(content);setDisplayName('?');
     }
-
-    if (role === 'assistant') {
-      displayName = env.AI_NAME;
-    }
-
-    setDisplayName(displayName);
-  }, [env.AI_NAME, role, user]);
+  }, [env.AI_NAME, role, user, content]);
 
   return (
-    <div className={`grid grid-cols-[30px_1fr] gap-5 p-5 ${role === 'assistant' ? 'bg-gray-600' : role === 'notice' ? 'bg-red-600' : ''}`}>
+    <div className={`grid grid-cols-[40px_1fr] gap-5 p-5 ${role === 'assistant' ? 'bg-gray-600' : role === 'notice' ? 'bg-red-600' : ''}`}>
       <div className="flex items-start justify-center">
         {role === 'user' && !!user && (
           <Image 
             src={user.picture} 
-            width={30}
-            height={30}
+            width={40}
+            height={40}
             alt="User avatar" 
             className="rounded-full shadow-md shadow-black/50"
           />
         )}
         {role === 'assistant' && (
-          <div className="flex items-center justify-center h-[30px] w-[30px] rounded-full shadow-md shadow-black/50 bg-gray-800">
+          <div className="flex items-center justify-center h-[40px] w-[40px] rounded-full shadow-md shadow-black/50 bg-gray-800">
             <FontAwesomeIcon icon={faRobot} className="text-emerald-200"/>
+          </div>
+        )}
+        {role === 'file' && (
+          <div className="flex items-center justify-center h-[40px] w-[40px] rounded-full shadow-md shadow-black/50 bg-gray-800">
+            <FontAwesomeIcon icon={faFile} className="text-red-400"/>
           </div>
         )}
       </div>
